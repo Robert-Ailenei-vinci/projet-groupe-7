3e7884c51965a8ad2a28ad1f5a1f970d4f7caab8
RobertAilenei <robert.ailenei@student.vinci.be>
2024-10-01 15:39:38 +0200

    added camera control with mouse

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 import * as THREE from 'three';
 const loader = new THREE.TextureLoader();
@@ -12,6 +13,14 @@ const texture = loader.load( '/part1/src/assets/Sun-0.jpg' );
 
 
 const renderer = new THREE.WebGLRenderer();
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // Optional: for smooth damping (inertia)
+controls.dampingFactor = 0.25; // Optional: adjust the damping factor
+controls.screenSpacePanning = false; // Optional: disable panning in screen space
+controls.minDistance = 1; // Optional: set the minimum zoom distance
+controls.maxDistance = 100; // Optional: set the maximum zoom distance
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
