8e36c39dba671d9684458e79fdd51d1595a7353d
RobertAilenei <robert.ailenei@student.vinci.be>
2024-10-01 15:26:29 +0200

    added sun light

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -16,17 +16,24 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
+
+//sun
 const geometry = new THREE.SphereGeometry();
 const material = new THREE.MeshBasicMaterial( { map:texture } );
 const sun = new THREE.Mesh( geometry, material );
 scene.add( sun );
+
+// Add a PointLight at the same position as the sun
+const sunLight = new THREE.PointLight( 0xffffff, 1, 100 );
+sunLight.position.copy(sun.position);
+scene.add( sunLight );
 
 camera.position.z = 5;
 
 function animate() {
 
 	sun.rotation.x += 0.001;
-
+	sunLight.position.copy(sun.position); // Ensure the light follows the sun
 	renderer.render( scene, camera );
 
 }
